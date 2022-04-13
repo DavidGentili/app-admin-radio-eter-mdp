e2973aae4f31801c6a770c5a3ec53788445a4348
DavidGentili <DavidGenntili@gmail.com>
2022-04-13 01:43:44 -0300

    table user styled

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ import './App.css'
 function App() {
 
     return (
-        <div className="App">
+        <div className="App grid">
             <Nav menuOptions={ menuOptions.filter(option => option.goTo === '/usuarios')}/>
             <UserPage />
 
--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,16 +1,45 @@
 import React from 'react'
 
 import Header from '../../componets/general/header/Header'
+import edit from '../../../assets/edit.png'
+
+import './users.css';
 
 const UserPage = () => {
     return (
-        <Header 
-        userName='Carlos' 
-        location={'Usuarios'} 
-        subMenus={[
-            {text: 'Usuarios', goTo: '/usuarios'},
-            {text: 'Otro', goTo: '/usuarios/otro'}    
-        ]} />
+        <>
+            <Header 
+            userName='Carlos' 
+            location={'Usuarios'} 
+            subMenus={[
+                {text: 'Usuarios', goTo: '/usuarios'},   
+            ]} />
+            <main>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Usuario</th>
+                            <th>Mail</th>
+                            <th>Nivel</th>
+                            <th>Estado</th>
+                            <th>Acciones</th>
+                        </tr>                        
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Carlos</th>
+                            <th>Carlos@mail.com</th>
+                            <th>Master</th>
+                            <th>Activo</th>
+                            <th><button><img src={edit} alt="Editar" /></button></th>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <button>Nuevo usuario</button>
+            </main>
+        </>
     )
 }
 
