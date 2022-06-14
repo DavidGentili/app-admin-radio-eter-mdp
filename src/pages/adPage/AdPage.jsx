801879488cd58ecbe503cc0c7712de3c6a813a13
DavidGentili <DavidGenntili@gmail.com>
2022-06-14 17:15:13 -0300

    Add ad page

--- src/pages/adPage/AdPage.jsx
+++ src/pages/adPage/AdPage.jsx
@@ -1,6 +1,9 @@
-import {React, useState} from 'react'
+import {React, useEffect, useState} from 'react'
 
 import ModalNewAd from '../../componets/modals/ModalNewAd';
+import LoadingPage from '../../componets/LoadingPage';
+import { getAds } from '../../services/ad';
+import SingleAd from '../../componets/SingleAd';
 
 import './adPage.css'
 
@@ -9,14 +12,43 @@ import './adPage.css'
 const AdPage = () => {
 
     const [openModal, setOpenModal] = useState(false);
+    const [loadingPage, setLoadingPage] = useState(true);
+    const [currentAd, setCurrentAd] = useState(null);
+    const [ads, setAds] = useState([]);
+
+    useEffect(() => {
+        getAds()
+        .then(({ data }) => {
+            setAds(data);
+            setLoadingPage(false)
+        })
+        .catch(e =>{
+        }) 
+    }, [])
 
     const closeModal = (e) => {
         setOpenModal(false);
     }
 
+    const selectAd = (ad) => {
+        return (e) => {
+            e.preventDefault();
+            setCurrentAd(ad);
+        }
+    } 
+
+    if(loadingPage)
+        return (<main className='adPage'> <LoadingPage/></main>)
+
     return (
         <>
             <main className='adPage'>
+                <div className="adPanel">
+                    { (ads.length > 0) &&
+                        ads.map(ad => <SingleAd key={ad.id} {...ad} selectAd={selectAd(ad)}/>)        
+                    }
+                </div>
+                
                 <button className='primaryBtn' onClick={(e) => {setOpenModal(true)}}> + </button>
             </main>
 
---
+++ src/services/ad.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+// 'https://api-radio-eter-mdp.herokuapp.com/ad'
+// 'http://localhost:9000/ad'
+
+const instance = axios.create({
+    baseURL: 'http://localhost:9000/ad',
+});
+
+const getHeaders = () => {
+    return {"authorization" : localStorage.getItem('userToken')}
+};
+
+
+const checkDataNewAd = (form) => {
+    const { name, file } = form
+    if(!name || typeof(name) !== 'string' || name.length === 0)
+        throw 'Debe ingresar un nombre correcto';
+    if(!file ||  !file.type || typeof(file.type) !== 'string' || !file.type.startsWith('image/'))
+        throw 'Error al cargar el archivo';
+}
+
+const createFormDataNewAd = (form) => {
+    const { name, file, type, altText, link } = form;
+    const formData = new FormData();
+    formData.append('ad', file);
+    formData.append('name', name);
+    formData.append('type', type);
+    formData.append('link', link);
+    formData.append('altText', altText);
+    return formData;
+}
+
+export async function createNewAd(form){
+    checkDataNewAd(form);
+    instance.post('', createFormDataNewAd(form), {
+        headers: { "Content-Type" : "multipart/form-data", ... getHeaders()},
+    })
+    .then(data => { return data})
+    .catch(e => console.log(e))   
+}
+
+export async function getAds(){
+    try{
+        return await instance.get('',{ headers : getHeaders() });
+    }catch(e){
+        return e.response.data.message
+    }
+}
