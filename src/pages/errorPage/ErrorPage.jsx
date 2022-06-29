e205dbd7a86e4e846a1ad2ba0e1d99e2e3f43664
DavidGentili <DavidGenntili@gmail.com>
2022-06-29 16:19:56 -0300

    lazy loading in html image

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -8,7 +8,7 @@ import './errorPage.css'
 const ErrorPage = (props) => {
   return (
     <main className='errorPage'>
-        <img src={errorPageImg} alt="error"/>
+        <img src={errorPageImg} loading='lazy' alt="error"/>
         <h2>Ha ocurrido un error</h2>
         <h3>Esta ruta no esta disponible, la misma se encuentra fuera de servicio o es invalida.</h3>
         <Link to='/' >Volver al panel de principal</Link>
