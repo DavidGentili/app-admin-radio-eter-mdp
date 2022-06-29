3e5ebe7ba0479b36ce74bd826037486d99c97491
DavidGentili <DavidGenntili@gmail.com>
2022-06-29 19:33:31 -0300

    new transmission page

--- src/pages/programPage/NewTransmissionPage.jsx
+++ src/pages/programPage/NewTransmissionPage.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import CustomInput from '../../componets/CustomInput'
+import CustomButton from '../../componets/CustomButton'
+
+import { createTransmission } from '../../services/transmissions'
 
 
 const NewTransmissionPage = () => {
-  return (
-    <form >
-        <CustomInput name='name' placeholder='Nombre' type='text' focus/>
-        <label htmlFor='startTransmission' className='label' >Fecha y Hora de inicio 
-            <input type="datetime-local" name="startTransmission" id="startTransmission" />
-        </label>
-        <label htmlFor='finishTransmission' className='label' >Fecha y Hora de Finalizacion 
-            <input type="datetime-local" name="finishTransmission" id="finishTransmission" />
-        </label>
-        
 
-    </form>
+    const [ loadingButton, setLoadingButton] = useState(false);
+    const [messageError, setMessageError] = useState('');
+
+    
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        setLoadingButton(true);
+        setMessageError('')
+        const form = Object.fromEntries(new FormData(e.target));
+        createTransmission(form)
+        .then((response) => {
+            console.log(response);
+            setLoadingButton(false);
+        })
+        .catch(e => {
+            setMessageError(e);
+            setLoadingButton(false);
+        })
+    }
+
+    return (
+        <form onSubmit={handlerSubmit}>
+            <CustomInput name='name' placeholder='Nombre' type='text' focus/>
+            <div className="hours">
+                <label htmlFor='startTransmission' className='label' >Fecha y Hora de inicio 
+                    <input type="datetime-local" name="startTransmission" id="startTransmission" />
+                </label>
+                <label htmlFor='finishTransmission' className='label' >Fecha y Hora de Finalizacion 
+                    <input type="datetime-local" name="finishTransmission" id="finishTransmission" />
+                </label>
+            </div>
+
+            <CustomButton text='Agregar transmision' type='primary' typeButton='submit' loading={loadingButton} disabled={loadingButton} />            
+
+            {messageError && <p className='messageError'>{messageError}</p>}
+
+        </form>
   )
 }
 
--- src/services/transmissions.js
+++ src/services/transmissions.js
@@ -7,3 +7,27 @@ export async function getTransmissions(){
         throw e.response ? e.response.data.message : e;
     }
 } 
+
+
+const getDataNewTransmission = (form) => {
+    const { name, startTransmission, finishTransmission } = form
+    if(!name || name.length < 3)
+        throw 'Debe ingresar un nombre valido'
+    if(!startTransmission || !finishTransmission)
+        throw 'Debe ingresar fechas validas'
+    const startDate = new Date(startTransmission);
+    const finishDate = new Date(finishTransmission);
+    if(finishDate < startDate)
+        throw 'Debe ingresar fechas validas'
+    return { name, startTransmission: startDate, finishTransmission: finishDate};
+}
+
+export async function createTransmission(form){
+    try{
+        const data = getDataNewTransmission(form);
+        return  await instance.post('/specialTransmission', data, { headers: getHeaders()});
+        // return await instance.get('/specialtransmission',{ headers : getHeaders() });
+    }catch(e){
+        throw e.response ? e.response.data.message : e;
+    }
+} 
