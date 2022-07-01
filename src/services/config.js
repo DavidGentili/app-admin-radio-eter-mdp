2d57853e6b06b71e0305f0f7af25c605fd0f1898
DavidGentili <DavidGenntili@gmail.com>
2022-07-01 14:42:04 -0300

    connect in heroku API

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const urlBase = 'https://api-radio-eter-mdp.herokuapp.com';
-const urlBase = 'http://localhost:9000';
+export const urlBase = 'https://api-radio-eter-mdp.herokuapp.com';
+// const urlBase = 'http://localhost:9000';
 
 export const instance = axios.create({
     baseURL: urlBase,
